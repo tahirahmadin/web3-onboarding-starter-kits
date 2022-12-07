6e5d725c70d5236ae19fd7a896c02bf584a8abb8
Tahir Ahmad <tahirahmad.in@gmail.com>
2022-12-07 14:42:03 +0530

    web3 auth configuration adds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,191 @@
+import { useEffect, useState } from "react";
+// import { Web3Auth } from "@web3auth/modal";
+import { Web3Auth } from "@web3auth/web3auth";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
+import RPC from "./web3RPC";
 import "./App.css";
-import Home from "./pages/Home";
+
+const clientId =
+  "BMkKHE4n2KgzLWFXDmpCVIpWMggQ8Pe8_4pRkbm9aNafKnn0WRlb1zoy6JlOh2nN2Aw54jIAbFbsAUut3tuJr8w"; // get from https://dashboard.web3auth.io
 
 function App() {
+  const [web3auth, setWeb3auth] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState("");
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0x13881",
+            rpcTarget: "https://rpc-mumbai.maticvigil.com/",
+          },
+        });
+
+        setWeb3auth(web3auth);
+        await web3auth.initModal();
+        setProvider(web3auth.provider);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+  };
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    setUserData(user);
+    console.log(user);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setProvider(null);
+  };
+
+  const getChainId = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const chainId = await rpc.getChainId();
+    console.log(chainId);
+  };
+  const getAccounts = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const address = await rpc.getAccounts();
+    setAddress(address);
+    console.log(address);
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    setBalance(balance);
+    console.log(balance);
+  };
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const receipt = await rpc.sendTransaction();
+    console.log(receipt);
+  };
+  const sendContractTransaction = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const receipt = await rpc.sendContractTransaction();
+    console.log(receipt);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const signedMessage = await rpc.signMessage();
+    console.log(signedMessage);
+  };
+
+  const getPrivateKey = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const privateKey = await rpc.getPrivateKey();
+    console.log(privateKey);
+  };
+  const loggedInView = (
+    <>
+      <button onClick={getUserInfo} className="card">
+        Get User Info
+      </button>
+      <button onClick={getChainId} className="card">
+        Get Chain ID
+      </button>
+      <button onClick={getAccounts} className="card">
+        Get Accounts
+      </button>
+      <button onClick={getBalance} className="card">
+        Get Balance
+      </button>
+      <button onClick={sendTransaction} className="card">
+        Send Transaction
+      </button>
+      <button onClick={sendContractTransaction} className="card">
+        Send Approve Transaction
+      </button>
+      <button onClick={signMessage} className="card">
+        Sign Message
+      </button>
+      <button onClick={getPrivateKey} className="card">
+        Get Private Key
+      </button>
+      <button onClick={logout} className="card">
+        Log Out
+      </button>
+
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}></p>
+      </div>
+    </>
+  );
+
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  );
+
   return (
-    <div className="App">
-      <Home />
+    <div className="container">
+      <h1 className="title">Web3Auth Example</h1>
+      <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+      address: {address}
+      <br />
+      balance: {balance}
+      <br />
+      user: {JSON.stringify(userData)}
     </div>
   );
 }
---
+++ src/web3RPC.js
@@ -0,0 +1,517 @@
+import Web3 from "web3";
+
+export default class RPC {
+  constructor(provider) {
+    this.provider = provider;
+  }
+
+  async getChainId() {
+    try {
+      const web3 = new Web3(this.provider);
+
+      // Get the connected Chain's ID
+      const chainId = await web3.eth.getChainId();
+
+      return chainId.toString();
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getAccounts() {
+    try {
+      const web3 = new Web3(this.provider);
+
+      // Get user's Ethereum public address
+      const address = (await web3.eth.getAccounts())[0];
+
+      return address;
+    } catch (error) {
+      return error;
+    }
+  }
+  async getBalance() {
+    try {
+      const web3 = new Web3(this.provider);
+
+      // Get user's Ethereum public address
+      const address = (await web3.eth.getAccounts())[0];
+
+      // Get user's balance in ether
+      const balance = web3.utils.fromWei(
+        await web3.eth.getBalance(address) // Balance is in wei
+      );
+
+      return balance;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async sendTransaction() {
+    try {
+      const web3 = new Web3(this.provider);
+
+      // Get user's Ethereum public address
+      const fromAddress = (await web3.eth.getAccounts())[0];
+
+      const destination = fromAddress;
+
+      const amount = web3.utils.toWei("0.001"); // Convert 1 ether to wei
+
+      // Submit transaction to the blockchain and wait for it to be mined
+      const receipt = await web3.eth.sendTransaction({
+        from: fromAddress,
+        to: destination,
+        value: amount,
+        maxPriorityFeePerGas: "5000000000", // Max priority fee per gas
+        maxFeePerGas: "6000000000000", // Max fee per gas
+      });
+
+      return receipt;
+    } catch (error) {
+      return error;
+    }
+  }
+  async sendContractTransaction() {
+    try {
+      let tokenConstant;
+
+      tokenConstant = {
+        abi: [
+          {
+            inputs: [
+              {
+                internalType: "string",
+                name: "name_",
+                type: "string",
+              },
+              {
+                internalType: "string",
+                name: "symbol_",
+                type: "string",
+              },
+            ],
+            stateMutability: "nonpayable",
+            type: "constructor",
+          },
+          {
+            anonymous: false,
+            inputs: [
+              {
+                indexed: true,
+                internalType: "address",
+                name: "owner",
+                type: "address",
+              },
+              {
+                indexed: true,
+                internalType: "address",
+                name: "spender",
+                type: "address",
+              },
+              {
+                indexed: false,
+                internalType: "uint256",
+                name: "value",
+                type: "uint256",
+              },
+            ],
+            name: "Approval",
+            type: "event",
+          },
+          {
+            inputs: [
+              {
+                internalType: "address",
+                name: "spender",
+                type: "address",
+              },
+              {
+                internalType: "uint256",
+                name: "amount",
+                type: "uint256",
+              },
+            ],
+            name: "approve",
+            outputs: [
+              {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+              },
+            ],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            inputs: [
+              {
+                internalType: "uint256",
+                name: "amount",
+                type: "uint256",
+              },
+            ],
+            name: "burn",
+            outputs: [
+              {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+              },
+            ],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            inputs: [
+              {
+                internalType: "address",
+                name: "account",
+                type: "address",
+              },
+              {
+                internalType: "uint256",
+                name: "amount",
+                type: "uint256",
+              },
+            ],
+            name: "burnFrom",
+            outputs: [
+              {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+              },
+            ],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            inputs: [
+              {
+                internalType: "address",
+                name: "spender",
+                type: "address",
+              },
+              {
+                internalType: "uint256",
+                name: "subtractedValue",
+                type: "uint256",
+              },
+            ],
+            name: "decreaseAllowance",
+            outputs: [
+              {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+              },
+            ],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            inputs: [
+              {
+                internalType: "address",
+                name: "spender",
+                type: "address",
+              },
+              {
+                internalType: "uint256",
+                name: "addedValue",
+                type: "uint256",
+              },
+            ],
+            name: "increaseAllowance",
+            outputs: [
+              {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+              },
+            ],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            anonymous: false,
+            inputs: [
+              {
+                indexed: false,
+                internalType: "address",
+                name: "account",
+                type: "address",
+              },
+            ],
+            name: "Paused",
+            type: "event",
+          },
+          {
+            inputs: [
+              {
+                internalType: "address",
+                name: "recipient",
+                type: "address",
+              },
+              {
+                internalType: "uint256",
+                name: "amount",
+                type: "uint256",
+              },
+            ],
+            name: "transfer",
+            outputs: [
+              {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+              },
+            ],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            anonymous: false,
+            inputs: [
+              {
+                indexed: true,
+                internalType: "address",
+                name: "from",
+                type: "address",
+              },
+              {
+                indexed: true,
+                internalType: "address",
+                name: "to",
+                type: "address",
+              },
+              {
+                indexed: false,
+                internalType: "uint256",
+                name: "value",
+                type: "uint256",
+              },
+            ],
+            name: "Transfer",
+            type: "event",
+          },
+          {
+            inputs: [
+              {
+                internalType: "address",
+                name: "sender",
+                type: "address",
+              },
+              {
+                internalType: "address",
+                name: "recipient",
+                type: "address",
+              },
+              {
+                internalType: "uint256",
+                name: "amount",
+                type: "uint256",
+              },
+            ],
+            name: "transferFrom",
+            outputs: [
+              {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+              },
+            ],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            anonymous: false,
+            inputs: [
+              {
+                indexed: false,
+                internalType: "address",
+                name: "account",
+                type: "address",
+              },
+            ],
+            name: "Unpaused",
+            type: "event",
+          },
+          {
+            inputs: [
+              {
+                internalType: "address",
+                name: "owner",
+                type: "address",
+              },
+              {
+                internalType: "address",
+                name: "spender",
+                type: "address",
+              },
+            ],
+            name: "allowance",
+            outputs: [
+              {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+              },
+            ],
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            inputs: [
+              {
+                internalType: "address",
+                name: "account",
+                type: "address",
+              },
+            ],
+            name: "balanceOf",
+            outputs: [
+              {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+              },
+            ],
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            inputs: [],
+            name: "decimals",
+            outputs: [
+              {
+                internalType: "uint8",
+                name: "",
+                type: "uint8",
+              },
+            ],
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            inputs: [],
+            name: "name",
+            outputs: [
+              {
+                internalType: "string",
+                name: "",
+                type: "string",
+              },
+            ],
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            inputs: [],
+            name: "paused",
+            outputs: [
+              {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+              },
+            ],
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            inputs: [],
+            name: "symbol",
+            outputs: [
+              {
+                internalType: "string",
+                name: "",
+                type: "string",
+              },
+            ],
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            inputs: [],
+            name: "totalSupply",
+            outputs: [
+              {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+              },
+            ],
+            stateMutability: "view",
+            type: "function",
+          },
+        ],
+      };
+
+      const web3 = new Web3(this.provider);
+
+      var tokenContract = new web3.eth.Contract(
+        tokenConstant.abi,
+        "0x07920F6d18464E56Da438D1ffF38f125C8AB90dD"
+      );
+
+      // Get user's Ethereum public address
+      const fromAddress = (await web3.eth.getAccounts())[0];
+
+      const destination = fromAddress;
+
+      //dsdf
+      const response = await tokenContract.methods
+        .approve(
+          "0xd1d25EAc33401b97568869564ee4ba6e259DCB35",
+          "100000000000000000000000000"
+        )
+        .send(
+          {
+            from: fromAddress,
+          },
+          function (error, transactionHash) {
+            if (transactionHash) {
+              console.log(transactionHash);
+
+              // setApproveCase(3);
+            } else {
+              console.log(error);
+            }
+          }
+        )
+        .on("receipt", async function (receipt) {
+          console.log(receipt);
+        })
+        .on("error", async function (error) {
+          console.log(error);
+        });
+      // Submit transaction to the blockchain and wait for it to be mined
+      //   const receipt = await web3.eth.sendTransaction({
+      //     from: fromAddress,
+      //     to: destination,
+      //     value: amount,
+      //     maxPriorityFeePerGas: "5000000000", // Max priority fee per gas
+      //     maxFeePerGas: "6000000000000", // Max fee per gas
+      //   });
+
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getPrivateKey() {
+    try {
+      const privateKey = await this.provider.request({
+        method: "eth_private_key",
+      });
+
+      return privateKey;
+    } catch (error) {
+      return error;
+    }
+  }
+}
